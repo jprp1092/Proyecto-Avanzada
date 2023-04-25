8ddbbdc2cf71efc1e8adbf338e2f9a395cf4d9d6
myusername <jquiros90650@ufide.ac.cr>
2023-04-25 08:54:46 -0600

    Detalles, Botones, datatables, forma botones de buscar

--- Proyecto_Avanzada/template/mail/contact.js
+++ Proyecto_Avanzada/template/mail/contact.js
@@ -27,18 +27,25 @@ $(function () {
                 success: function () {
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
+                        .append("</button>");
                     $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your message has been sent. </strong>");
                     $('#success > .alert-success')
-                            .append('</div>');
+                        .append('</div>');
                     $('#contactForm').trigger("reset");
+
+                    // Show success message with SweetAlert
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Success!',
+                        text: 'Tu mensaje ha sido enviado!',
+                    });
                 },
                 error: function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                        .append("</button>");
+                    $('#success > .alert-danger').append($("<strong>").text("Disculpas " + name + ", Parece que nuestro servidor no esta funcionando bien. Intentalo en otro momento!"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
